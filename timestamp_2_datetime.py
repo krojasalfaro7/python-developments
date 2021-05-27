f32ec4a6535f1d4e0a6f9c09c8c5d618d52f5525
Kevin Rojas <krojas.alfaro7@gmail.com>
2021-05-27 11:38:55 -0400

    bak

--- timestamp_2_datetime.py
+++ timestamp_2_datetime.py
@@ -7,10 +7,17 @@ from datetime import datetime
 timestamp = 1545730073
 dt_object = datetime.fromtimestamp(timestamp)
 
-print("dt_object =", dt_object)
+# print("dt_object =", dt_object)
 
 # current date and time
 now = datetime.now()
 
 timestamp = datetime.timestamp(now)
-print("timestamp =", timestamp)
+# print("timestamp =", timestamp)
+
+# Diferencia entre date y timestamp
+
+timestamp = 1607025879
+message_date = datetime.fromtimestamp(int(timestamp)).date()
+result = datetime.now().date() - message_date
+print(result.days)
